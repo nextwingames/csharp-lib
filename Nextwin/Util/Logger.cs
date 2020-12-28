47a721dd8ac425ada269d83648beeca3ee3a6800
DESKTOP-DRC0DO8\graction <dhk0561@naver.com>
2020-12-28 17:03:05 +0900

    [feat] Log for different platforms

---
+++ Nextwin/Util/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Nextwin.Util
+{
+    public static class Logger
+    {
+        public static void Log<T>(T message)
+        {
+            Log(message, 2);
+        }
+
+        public static void Log(byte[] bytes)
+        {
+            Log(ArrayStringConverter.BytesToString(bytes), 2);
+        }
+
+        private static void Log<T>(T message, int stackFrame)
+        {
+            try
+            {
+                Debug.Log(message);
+            }
+            catch(Exception)
+            {
+                var frame = new System.Diagnostics.StackFrame(stackFrame);
+                string format = $"[{DateTime.Now:HH:mm:ss}] {message}\n{frame.GetMethod().DeclaringType.Name}\n";
+                Console.WriteLine(format);
+            }
+        }
+    }
+}
