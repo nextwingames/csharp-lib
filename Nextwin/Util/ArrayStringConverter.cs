dc0bf2d65e0a30bd771ba2a3a3c589ca00f6731e
DESKTOP-DRC0DO8\graction <dhk0561@naver.com>
2020-12-28 17:03:33 +0900

    [feat] Convert array to string

---
+++ Nextwin/Util/ArrayStringConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nextwin.Util
+{
+    public static class ArrayStringConverter
+    {
+        public static string BytesToString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder("{ ");
+            foreach(var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public static string IntsToString(int[] ints)
+        {
+            StringBuilder sb = new StringBuilder("{ ");
+            foreach(var b in ints)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public static string FloatsToString(float[] floats)
+        {
+            StringBuilder sb = new StringBuilder("{ ");
+            foreach(var b in floats)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+    }
+}
