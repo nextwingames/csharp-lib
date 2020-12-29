62abf1496f394a10e158339da3d566d5e24f8a84
DESKTOP-DRC0DO8\graction <dhk0561@naver.com>
2020-12-29 16:58:38 +0900

    [feat] Add serializable class

---
+++ Nextwin/Protocol/SerializableData.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System.Collections.Generic;
+
+namespace Nextwin.Protocol
+{
+    [MessagePackObject]
+    public class SerializableData<TKey>
+    {
+        [Key(0)]
+        public Dictionary<TKey, object> DataMap { get; set; }
+
+        public SerializableData()
+        {
+            DataMap = new Dictionary<TKey, object>();
+        }
+
+        public void Add(TKey key, object value)
+        {
+            DataMap.Add(key, value);
+        }
+    }
+}
